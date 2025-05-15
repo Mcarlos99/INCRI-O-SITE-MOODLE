25a22bb714aa2bfd0696122591521952b85a4c4f
Mcarlos99 <raposa_killer@hotmail.com>
2025-05-15 12:07:40 -0300

    pl

--- polo_config.php
+++ polo_config.php
@@ -1,33 +1,37 @@
 <?php
 // Configuração dos polos e suas respectivas instâncias Moodle
-$POLO_CONFIG = [
-    'tucurui' => [
-        'name' => 'Tucuruí',
-        'moodle_url' => 'https://tucurui.imepedu.com.br',
-        'api_token' => '051a62d5f60167246607b195a9630d3b', // Token da instância Moodle de Tucuruí
-        'description' => 'Polo de Educação Superior de Tucuruí',
-        'address' => 'Av. Principal, 1234 - Centro, Tucuruí - PA'
-    ],
-    'breu-branco' => [
-        'name' => 'Breu Branco',
-        'moodle_url' => 'https://breubranco.imepedu.com.br',
-        'api_token' => 'token_do_polo_breu_branco_aqui', // Token da instância Moodle de Breu Branco
-        'description' => 'Polo de Educação Superior de Breu Branco',
-        'address' => 'Rua Principal, 567 - Centro, Breu Branco - PA'
-    ],
-    'igarape-miri' => [
-        'name' => 'Igarapé-Miri',
-        'moodle_url' => 'https://igarape.imepedu.com.br', 
-        'api_token' => '051a62d5f60167246607b195a9630d3b', // Token da instância Moodle de Igarapé-Miri
-        'description' => 'Polo de Educação Superior de Igarapé-Miri',
-        'address' => 'Tv. Principal, 890 - Centro, Igarapé-Miri - PA'
-    ],
-    'moju' => [
-        'name' => 'Moju',
-        'moodle_url' => 'https://moju.imepedu.com.br',
-        'api_token' => 'token_do_polo_moju_aqui', // Token da instância Moodle de Moju
-        'description' => 'Polo de Educação Superior de Moju',
-        'address' => 'Av. das Palmeiras, 123 - Centro, Moju - PA'
-    ]
-];
+$POLO_CONFIG = array (
+  'tucurui' => 
+  array (
+    'name' => 'Tucuruí',
+    'moodle_url' => 'https://tucurui.imepedu.com.br',
+    'api_token' => 'xxx',
+    'description' => 'Polo de Educação Superior de Tucuruí',
+    'address' => 'Av. Principal, 1234 - Centro, Tucuruí - PA',
+  ),
+  'breu-branco' => 
+  array (
+    'name' => 'Breu Branco',
+    'moodle_url' => 'https://breubranco.imepedu.com.br',
+    'api_token' => '0441051a5b5bc8968f3e65ff7d45c3de',
+    'description' => 'Polo de Educação Superior de Breu Branco',
+    'address' => 'Rua Principal, 567 - Centro, Breu Branco - PA',
+  ),
+  'igarape-miri' => 
+  array (
+    'name' => 'Igarapé-Miri',
+    'moodle_url' => 'https://igarape.imepedu.com.br',
+    'api_token' => '051a62d5f60167246607b195a9630d3b',
+    'description' => 'Polo de Educação Superior de Igarapé-Miri',
+    'address' => 'Tv. Principal, 890 - Centro, Igarapé-Miri - PA',
+  ),
+  'moju' => 
+  array (
+    'name' => 'Moju',
+    'moodle_url' => 'https://moju.imepedu.com.br',
+    'api_token' => 'xxx',
+    'description' => 'Polo de Educação Superior de Moju',
+    'address' => 'Av. das Palmeiras, 123 - Centro, Moju - PA',
+  ),
+);
 ?>

71f53ecb148fd56ee3980f3f45ddc430a074d6ca
Mcarlos99 <raposa_killer@hotmail.com>
2025-07-31 14:32:32 -0300

    OK

--- polo_config.php
+++ polo_config.php
@@ -90,27 +90,126 @@ $POLO_CONFIG = array (
       ),
     ),
   ),
-  'ava' => 
+    'repartimento' => 
   array (
-    'name' => 'AVA - Ambiente Virtual de Aprendizagem',
-    'moodle_url' => 'https://ava.imepedu.com.br',
-    'api_token' => '9eeee9d48903c760bc224b61e0547e4e',
+    'name' => 'Novo Repartimento',
+    'moodle_url' => 'https://repartimento.imepedu.com.br',
+    'api_token' => '25c578c6ec5d4c1b75547ea52a6fcf7c',
+    'description' => 'Polo de Educação Superior de Novo Repartimento',
+    'address' => 'Centro, Novo Repartimento - PA',
+    'hierarchical_navigation' => true, // Navegação hierárquica - categorias > subcategorias
+    'course_prices' => array(
+      26 => array( // Técnico em Enfermagem 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      27 => array( // Técnico em Eletromecânica 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      28 => array( // Técnico em Eletrotécnica 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      29 => array( // Técnico em Segurança do Trabalho
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      33 => array( // NR'S 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      'default' => array(
+          'price' => 1500.00,
+          'duration' => '12 meses',
+          'installments' => '12x de R$ 125,00',
+      ),
+    ),
+  ),
+ 'bioquality' => 
+  array (
+    'name' => 'BIOQUALITY - INVISTA EM RESULTADOS',
+    'moodle_url' => 'https://bioquality.imepedu.com.br/ava',
+    'api_token' => 'a4f1bd19f54bff53ad028085c34cad48',
+    'description' => 'Polo de Educação Superior de Breu Branco',
+    'address' => 'Rua Sapucaia, n36, Primavera, Parauapebas 68515-000',
+    'hierarchical_navigation' => true, // Navegação hierárquica - categorias > subcategorias
+    'course_prices' => array(
+      26 => array( // Técnico em Enfermagem 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      27 => array( // Técnico em Eletromecânica 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      28 => array( // Técnico em Eletrotécnica 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      29 => array( // Técnico em Segurança do Trabalho
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      33 => array( // NR'S 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      'default' => array(
+          'price' => 1500.00,
+          'duration' => '12 meses',
+          'installments' => '12x de R$ 125,00',
+      ),
+    ),
+  ),
+    'ava' => 
+  array (
+    'name' => 'AVA',
+    'moodle_url' => 'https://ava.imepedu.com.br/',
+    'api_token' => '0c8ef233994c5ccfe22d6ed7d4e86a05',
     'description' => 'AVA - Ambiente Virtual de Aprendizagem',
     'address' => 'BRASIL',
-    'hierarchical_navigation' => true, // Navegação simples - só categorias
-    'course_prices' => 
-    array (
-      1 => 
-      array (
-        'price' => 9.0,
-        'duration' => '6 meses',
-        'installments' => '6x de R$ 44,50',
+    'hierarchical_navigation' => true, // Navegação hierárquica - categorias > subcategorias
+    'course_prices' => array(
+      26 => array( // Técnico em Enfermagem 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
       ),
-      'default' => 
-      array (
-        'price' => 12.0,
-        'duration' => '6 meses',
-        'installments' => '6x de R$ 44,50',
+      27 => array( // Técnico em Eletromecânica 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      28 => array( // Técnico em Eletrotécnica 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      29 => array( // Técnico em Segurança do Trabalho
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      33 => array( // NR'S 
+          'price' => 2500.00,
+          'duration' => '18 meses',
+          'installments' => '18x de R$ 138,89',
+      ),
+      'default' => array(
+          'price' => 1500.00,
+          'duration' => '12 meses',
+          'installments' => '12x de R$ 125,00',
       ),
     ),
   ),
